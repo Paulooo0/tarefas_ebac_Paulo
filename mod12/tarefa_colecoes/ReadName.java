e0b04d90c2ffb02beba535343643673894348b27
Paulooo0 <paulohenrique.n.s00@gmail.com>
2023-09-29 13:51:40 -0300

    ADD: module 12 ADD: ReadName.java

---
+++ mod12/tarefa_colecoes/ReadName.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReadName {
+    public static void main(String[] args) {
+        String names = getNames();
+        splitNames(names);
+    }
+
+    private static String getNames() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Digite os nommes separados por virgula\n");
+        String names = s.nextLine();
+
+        return names;
+    }
+
+    private static void splitNames(String names) {
+        String[] namesArray = names.split(",");
+        Arrays.sort(namesArray);
+
+        System.out.println("\n***************************");
+        Arrays.stream(namesArray).forEach(System.out::println);
+        System.out.println("***************************");
+    }
+}
