952a2367072db8c8dcbe28c659e814887a8847f7
Paulooo0 <paulohenrique.n.s00@gmail.com>
2023-10-20 13:33:18 -0300

    ADD: module 22

---
+++ mod22/tarefa_streams/App.java
@@ -0,0 +1,22 @@
+package tarefa_streams;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import tarefa_streams.people.GetPeopleList;
+import tarefa_streams.people.People;
+
+public class App extends GetPeopleList {
+    public static void main(String[] args) {
+        String input = getInput();
+        List<People> peopleList = getPeopleList(input);
+
+        Map<String, String> map = peopleList.stream()
+        .filter(p -> p.getGender().equals("mulher"))
+        .collect(Collectors.toMap(People::getName, People::getGender));
+
+        System.out.println("\nMulheres:");
+        map.forEach((k, v) -> System.out.println(k));    
+    }
+}
---
+++ mod22/tarefa_streams/people/GetPeopleList.java
@@ -0,0 +1,36 @@
+package tarefa_streams.people;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class GetPeopleList {
+
+    public static String getInput() {
+        System.out.println("\nInsira os dados (ex: Pedro-homem,Claudia-mulher,...):");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        scanner.close();
+
+        return input;
+    }
+
+    public static List<People> getPeopleList(String input) {
+        String[] peopleArray = input.split(",");
+
+        List<String> peopleList = Arrays.asList(peopleArray);
+
+        People people = new People();
+        peopleList.stream()
+            .forEach(p -> {
+                String[] person = p.split("-");
+
+                String name = person[0];
+                String gender = person[1];
+
+                people.setName(name);
+                people.setGender(gender);
+            });
+
+        return Arrays.asList(people);
+    }
+}
